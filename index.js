061de526e2127c49f9104bbac23e10bfdffa6b1c
David Bae <baeddavid@gmail.com>
2019-09-10 18:31:23 -0500

    basic socket io setup done

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket) {
     console.log('a user connected');
+    socket.on('disconnect', function() {
+        console.log('a user disconnected');
+    });
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        console.log('message: ' + msg);
+    });
+})
+
+io.emit('some event', { for: 'everyone' });
+
+io.on('connection', function(socket) {
+    socket.broadcast.emit('hi');
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
 });
 
 http.listen(3000, function() {
